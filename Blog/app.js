f6b1c58e09245c18e822986abcac42d1b85ae13d
VallistA <vallista@tistory.com>
2017-02-15 15:45:04 +0900

    development start 'login'

--- Blog/app.js
+++ Blog/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var board = require('./routes/board');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -23,9 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/scripts', express.static(__dirname + '/public/javascripts'));
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/board', board);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
